36770f230734f29a70bf258b2418e1879d47cedc
Federico Maidán <fede.maidan@gmail.com>
2019-03-04 16:39:05 -0300

    masiva por cuenta id

--- src/AppBundle/Command/MercadoLibre/PublicacionMasivaEbayMLCommand.php
+++ src/AppBundle/Command/MercadoLibre/PublicacionMasivaEbayMLCommand.php
@@ -47,7 +47,7 @@ class PublicacionMasivaEbayMLCommand extends ContainerAwareCommand
         if (($handle = fopen($archivo, "r")) !== FALSE) {
 	        while (($data = fgetcsv($handle, 10000, ",")) !== FALSE) {
                 
-				if ($this->row > 0 && ($data[17] == "z" ||  $data[17] == "j" || $data[17] == "jj" || $data[17] == "jjj")) {
+				if ($this->row > 0)) {
 	           	    $this->cargarPublicacion($data, $cuenta_id);
                 }
 	           	$this->row++;
